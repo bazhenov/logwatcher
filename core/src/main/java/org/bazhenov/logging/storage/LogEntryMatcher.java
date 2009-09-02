36677ee52db588c0dea9791a856f061b1861eae9
Denis Bazhenov <bazhenov@farpost.com>
2009-09-02 23:16:29 +1100

    Документация к классу LogEntryMatcher

--- core/src/main/java/org/bazhenov/logging/storage/LogEntryMatcher.java
+++ core/src/main/java/org/bazhenov/logging/storage/LogEntryMatcher.java
@@ -2,6 +2,9 @@ package org.bazhenov.logging.storage;
 
 import org.bazhenov.logging.AggregatedLogEntry;
 
+/**
+ * Имплементации этого интерфейса фильтруют обьекты типа {@link AggregatedLogEntry}
+ */
 public interface LogEntryMatcher {
 
 	boolean isMatch(AggregatedLogEntry entry);
