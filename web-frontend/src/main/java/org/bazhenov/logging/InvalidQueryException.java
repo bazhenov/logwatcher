d035ee4a95ca08b3e95fca51aeced48a82b9e31c
Denis Bazhenov <bazhenov@farpost.com>
2009-12-02 21:04:00 +1000

    added: QueryParser имплементация парсера запросов

---
+++ web-frontend/src/main/java/org/bazhenov/logging/InvalidQueryException.java
@@ -0,0 +1,12 @@
+package org.bazhenov.logging;
+
+/**
+ * Эта исключительная ситуация генерируется объектами типа {@link QueryParser} в случае
+ * если запрос в некорректном формате.
+ */
+public class InvalidQueryException extends Exception {
+
+	public InvalidQueryException(String message) {
+		super(message);
+	}
+}
---
+++ web-frontend/src/main/java/org/bazhenov/logging/QueryParser.java
@@ -0,0 +1,29 @@
+package org.bazhenov.logging;
+
+import java.util.*;
+
+public class QueryParser {
+
+	public Map<String, String> parse(String query) throws InvalidQueryException {
+		String lastOperation = null;
+		StringBuffer operand = new StringBuffer();
+		Map<String, String> operations = new HashMap<String, String>();
+		for ( String part : query.replace(":", ": ").split(" ") ) {
+			if ( part.endsWith(":") ) {
+				if ( lastOperation != null ) {
+					if ( operand.length() <= 0 ) {
+						throw new InvalidQueryException(
+							"Operation '" + lastOperation + "' do not have operand");
+					}
+					operations.put(lastOperation, operand.toString().trim());
+				}
+				lastOperation = part.substring(0, part.length() - 1);
+				operand = new StringBuffer();
+			} else if ( part.length() > 0 ) {
+				operand.append(part).append(' ');
+			}
+		}
+		operations.put(lastOperation, operand.toString().trim());
+		return operations;
+	}
+}
