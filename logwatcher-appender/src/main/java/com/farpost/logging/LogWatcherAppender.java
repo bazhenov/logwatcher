5ff730943da786334eab865cdc08767ea071eece
Denis Bazhenov <bazhenov@farpost.com>
2010-02-04 17:25:15 +1000

    added: remote appender теперь под крылом log watcher'а

---
+++ logwatcher-appender/src/main/java/com/farpost/logging/LogWatcherAppender.java
@@ -0,0 +1,133 @@
+package com.farpost.logging;
+
+import com.farpost.timepoint.DateTime;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
+import org.bazhenov.logging.*;
+import org.bazhenov.logging.marshalling.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * It is an implementation of Log4j appender that sends
+ * log message to the log system of Bazhenov Denis
+ * <p/>
+ * Configuration:
+ * <appender name="remote" class="com.farpost.logging.LogWatcherAppender">
+ *   <param name="address" value="aux2.srv.loc:6578" />
+ *   <param name="threshold" value="WARNING" />
+ *   <param name="applicationId" value="serviceName" />
+ * </appender>
+ */
+public class LogWatcherAppender extends AppenderSkeleton {
+
+	private Collection clients = new LinkedList();
+	private int port = 6578;
+	private DatagramSocket socket;
+	private InetAddress address;
+	private String applicationId = "anonymous";
+	private Marshaller marshaller = new JDomMarshaller();
+
+	public String getAddress() {
+		return address.getHostName();
+	}
+
+	public String getApplicationId() {
+		return applicationId;
+	}
+
+	public void setApplicationId(String applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public void setAddress(String address) {
+		try {
+			String parts[] = address.split(":");
+			this.address = InetAddress.getByName(parts[0]);
+			this.port = Integer.parseInt(parts[1]);
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void sendMessage(int port, String message) throws IOException {
+		DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length());
+		packet.setAddress(address);
+		packet.setPort(port);
+		socket.send(packet);
+	}
+
+	private Severity getSererity(Level level) {
+		if (level == Level.OFF) {
+			return Severity.error;
+		} else if (level == Level.FATAL) {
+			return Severity.error;
+		} else if (level == Level.ERROR) {
+			return Severity.error;
+		} else if (level == Level.WARN) {
+			return Severity.warning;
+		} else if (level == Level.INFO) {
+			return Severity.info;
+		} else if (level == Level.DEBUG) {
+			return Severity.debug;
+		}
+		return Severity.trace;
+	}
+
+	private Cause constructCause(Throwable t) {
+		StringWriter buffer = new StringWriter();
+		t.printStackTrace(new PrintWriter(buffer));
+		Cause cause = t.getCause() == null
+			? null
+			:constructCause(t.getCause());
+		return new Cause(t.getClass().getSimpleName(), t.getMessage(), buffer.toString(), cause);
+	}
+
+	private String calculateChecksum(String message, String location) {
+		return message + location;
+	}
+
+	public void activateOptions() {
+		try {
+			socket = new DatagramSocket();
+		} catch (SocketException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected void append(LoggingEvent event) {
+		String message = event.getRenderedMessage();
+		Level level = event.getLevel();
+		String location = event.getLocationInformation().getClassName();
+		Severity severity = getSererity(level);
+		DateTime now = DateTime.now();
+		String checksum = calculateChecksum(message, location);
+
+		LogEntry entry;
+		if (event.getThrowableInformation() != null) {
+			Throwable t = event.getThrowableInformation().getThrowable();
+			entry = new LogEntry(now, location, message, severity, checksum, applicationId, null, constructCause(t));
+		} else {
+			entry = new LogEntry(now, location, message, severity, checksum, applicationId, null);
+		}
+		try {
+			String stringMessage = marshaller.marshall(entry);
+			sendMessage(port, stringMessage);
+		} catch (MarshallerException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void close() {
+		socket.close();
+	}
+
+	public boolean requiresLayout() {
+		return true;
+	}
+}
---
+++ logwatcher-appender/src/test/java/com/farpost/logging/LogWatcherAppenderTest.java
@@ -0,0 +1,46 @@
+package com.farpost.logging;
+
+import org.apache.log4j.*;
+import org.apache.log4j.spi.LoggingEvent;
+import org.bazhenov.logging.LogEntry;
+import org.bazhenov.logging.marshalling.*;
+import org.bazhenov.logging.transport.*;
+import org.testng.annotations.Test;
+
+import java.net.SocketException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LogWatcherAppenderTest {
+
+	@Test
+	public void appenderShouldSendUdpMessages() throws SocketException, TransportException,
+		InterruptedException, MarshallerException {
+
+		Marshaller marshaller = new JDomMarshaller();
+		BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
+		Transport t = new UdpTransport(6589, new QueueAppendListener(queue));
+		LogWatcherAppender appender = new LogWatcherAppender();
+		appender.setAddress("127.0.0.1:6589");
+		String applicationId = "foobar";
+		appender.setApplicationId(applicationId);
+		appender.activateOptions();
+		t.start();
+
+		Throwable cause = new RuntimeException("Ooops");
+		String message = "Message";
+		LoggingEvent e = new LoggingEvent("category", Category.getRoot(), Priority.DEBUG, message, cause);
+		appender.doAppend(e);
+
+		String packet = queue.take();
+		LogEntry entry = marshaller.unmarshall(packet);
+
+		assertThat(entry.getMessage(), equalTo(message));
+		assertThat(entry.getApplicationId(), equalTo(applicationId));
+		assertThat(entry.getCause().getMessage(), equalTo("Ooops"));
+		assertThat(entry.getCause().getType(), equalTo(RuntimeException.class.getSimpleName()));
+	}
+}
