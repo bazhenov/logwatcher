06f46dfc6bd2e8a03000ed6e09477c6038833685
Denis Bazhenov <dotsid@gmail.com>
2010-09-05 20:38:32 +1100

    имплементация Serializable компараторами

--- logging-core/src/main/java/org/bazhenov/logging/AttributeValueComparator.java
+++ logging-core/src/main/java/org/bazhenov/logging/AttributeValueComparator.java
@@ -1,8 +1,9 @@
 package org.bazhenov.logging;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class AttributeValueComparator implements Comparator<AttributeValue> {
+public class AttributeValueComparator implements Comparator<AttributeValue>, Serializable {
 
 	public int compare(AttributeValue attr1, AttributeValue attr2) {
 		return attr2.getCount() - attr1.getCount();
--- logging-core/src/main/java/org/bazhenov/logging/ByOccurenceCountComparator.java
+++ logging-core/src/main/java/org/bazhenov/logging/ByOccurenceCountComparator.java
@@ -1,8 +1,9 @@
 package org.bazhenov.logging;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class ByOccurenceCountComparator implements Comparator<AggregatedEntry> {
+public class ByOccurenceCountComparator implements Comparator<AggregatedEntry>, Serializable {
 
 	public int compare(AggregatedEntry o1, AggregatedEntry o2) {
 		return o2.getCount() - o1.getCount();
