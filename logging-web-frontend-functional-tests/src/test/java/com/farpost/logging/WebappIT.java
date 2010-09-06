9a7b7f0277dfa11c65e10ac3ba26ae1d880e6cb1
Denis Bazhenov <dotsid@gmail.com>
2010-09-07 08:19:19 +1100

    отрефакторен POM для функционального тестирования

--- logging-web-frontend-functional-tests/src/test/java/com/farpost/logging/WebappIT.java
+++ logging-web-frontend-functional-tests/src/test/java/com/farpost/logging/WebappIT.java
@@ -13,7 +13,6 @@ public class WebappIT {
 	@Test
 	public void testCallIndexPage() throws Exception {
 		URL url = new URL("http://localhost:8080/");
-		Thread.sleep(20000);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.connect();
 		assertThat(connection.getResponseCode(), equalTo(200));
