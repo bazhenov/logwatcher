0d623b81ca491e1f40d8606c414d41e3a1a06c47
Denis Bazhenov <bazhenov@farpost.com>
2010-09-07 18:29:46 +1100

    modified: изменения связанные с циклом функционального тестирования
    - перенес jetty по-умолчанию на порт 8181
    - вынес system property "it.port" - порт на котором запускается приложение для функциональных тестов

--- logging-web-frontend-functional-tests/src/test/java/com/farpost/logging/WebappIT.java
+++ logging-web-frontend-functional-tests/src/test/java/com/farpost/logging/WebappIT.java
@@ -2,17 +2,26 @@ package com.farpost.logging;
 
 import org.testng.annotations.Test;
 
-import java.net.URL;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.System.getProperty;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class WebappIT {
 
+	private String applicationUrl;
+
+	public WebappIT() {
+		int port = parseInt(getProperty("it.port"));
+		applicationUrl = "http://localhost:" + port + "/";
+	}
+
 	@Test
 	public void testCallIndexPage() throws Exception {
-		URL url = new URL("http://localhost:8080/");
+		URL url = new URL(applicationUrl);
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.connect();
 		assertThat(connection.getResponseCode(), equalTo(200));
