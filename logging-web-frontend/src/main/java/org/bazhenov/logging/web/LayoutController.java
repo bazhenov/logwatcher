15cb8ba776cfae52da0d7f4f2a45d2b20cbce5f6
Denis Bazhenov <dotsid@gmail.com>
2010-09-15 14:26:46 +1100

    Перенес layout в отдельный контроллер

---
+++ logging-web-frontend/src/main/java/org/bazhenov/logging/web/LayoutController.java
@@ -0,0 +1,17 @@
+package org.bazhenov.logging.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class LayoutController {
+
+	@RequestMapping("/layout")
+	public String doLayout(ModelMap map) {
+		map.put("date", new Date());
+		return "layout/main";
+	}
+}
