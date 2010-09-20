7f6236ae28c130a89cd835d24989582e642540a1
Denis Bazhenov <dotsid@gmail.com>
2010-09-20 22:45:41 +1100

    работа над инфраструктурой функционального тестирования
    - увеличил производительность HtmlUnit за счет отключения процессинга JS и CSS

--- logging-web-frontend-functional-tests/src/test/java/com/farpost/logging/WebAppIT.java
+++ logging-web-frontend-functional-tests/src/test/java/com/farpost/logging/WebAppIT.java
@@ -1,27 +1,34 @@
 package com.farpost.logging;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 import static com.farpost.htmlunit.Matchers.containsText;
-import static java.lang.Integer.parseInt;
 import static java.lang.System.getProperty;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class WebAppIT {
 
 	private String applicationUrl;
-	private WebClient browser = new WebClient();
+	private WebClient browser = new WebClient(BrowserVersion.FIREFOX_3);
 
-	public WebAppIT() {
-		int port = parseInt(getProperty("it.port"));
-		applicationUrl = "http://localhost:" + port + "/";
+	public WebAppIT() throws IOException {
+		applicationUrl = getProperty("it.location");
+		if (applicationUrl == null || applicationUrl.equals("")) {
+			throw new RuntimeException("it.location property should be given");
+		}
+		browser.setJavaScriptEnabled(false);
+		browser.setCssEnabled(false);
+		browser.setTimeout(5000);
 	}
 
 	@Test
 	public void testCallIndexPage() throws Exception {
-		HtmlPage page = browser.getPage(applicationUrl);
+		HtmlPage page = browser.getPage(applicationUrl + "/dashboard");
 		assertThat(page, containsText("frontend"));
 		HtmlPage frontendPage = page.getAnchorByText("frontend").click();
 		assertThat(frontendPage, containsText("frontend"));
