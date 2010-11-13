bbeb3accb7881796300edf4bfbe9e7b69c1d1976
Denis Bazhenov <dotsid@gmail.com>
2010-11-13 21:10:38 +1000

    переименован пакет logging-logback-appender -> logwatcher-logback-appender

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://team.dev.loc/repository/download/bt14/.lastSuccessful/logging-web-frontend.war?guest=1 -O logging-web-frontend.war
-FILE_NAME=logwatcher-`date +%Y%m%d-%H%M`.war
-scp -S deploy.py logging-web-frontend.war vhost+logwatcher:$FILE_NAME
-rm logging-web-frontend.war
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-$packet = '<?xml version="1.0" encoding="utf8"?>
-<logEntry xmlns="http://logging.farpost.com/schema/v1.1" date="2009-12-11T17:03:26+10:00" checksum="e85a67d17ede2673abda44c99c093a77"><message>Rolling back transaction with (1 resources)</message><application id="frontend"/><attributes><attribute name="machine">aux2.srv.loc</attribute><attribute name="group">slrTransaction</attribute></attributes><group name=""/><severity name="warning"/></logEntry>';
-echo socket_sendto($socket,  $packet, strlen($packet), 0, "127.0.0.1", 6578);
