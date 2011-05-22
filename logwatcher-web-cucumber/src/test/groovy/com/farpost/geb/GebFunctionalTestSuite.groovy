e7ee696e655d2e32cc0ed85cc604e85845ab4f10
Alexander Zolotov <zolotov@farpost.com>
2011-05-22 22:02:03 +1100

    Complex functional test

--- logwatcher-web-cucumber/src/test/groovy/com/farpost/geb/GebFunctionalTestSuite.groovy
+++ logwatcher-web-cucumber/src/test/groovy/com/farpost/geb/GebFunctionalTestSuite.groovy
@@ -1,5 +1,9 @@
 package com.farpost.geb
 
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.Appender
+import com.farpost.logwatcher.logback.LogWatcherAppender
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
@@ -10,6 +14,9 @@ abstract class GebFunctionalTestSuite {
 
 	protected WebDriver driver
 	protected final String applicationUri = "http://localhost:8080"
+	private int port = 6578
+	private String applicationId = "foobar"
+	private HashMap<String, Appender<ILoggingEvent>> appenders = new HashMap<String, Appender<ILoggingEvent>>()
 
 	@BeforeSuite
 	public void setUp() {
@@ -18,8 +25,30 @@ abstract class GebFunctionalTestSuite {
 
 	@AfterSuite
 	public void tearDown() {
-		if(!Boolean.getBoolean("preventClose")) {
+		if (!Boolean.getBoolean("preventClose")) {
 			driver.close()
 		}
+	}
+
+	private Appender<ILoggingEvent> getAppender(String applicationId) {
+		if (appenders.containsKey(applicationId)) {
+			return appenders.get(applicationId)
+		} else {
+			LogWatcherAppender appender = new LogWatcherAppender()
+			appender.setAddress("0.0.0.0:" + port)
+			appender.setApplicationId(applicationId)
+			appender.start()
+			appenders.put(applicationId, appender)
+			return appender
+		}
+
+	}
+
+	protected Logger getLogger(String applicationId) {
+		Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(applicationId)
+		if (logger.getAppender(applicationId) == null) {
+			logger.addAppender(getAppender(applicationId))
+		}
+		return logger
 	}
 }
--- logwatcher-web-cucumber/src/test/groovy/com/farpost/geb/SearchTest.groovy
+++ logwatcher-web-cucumber/src/test/groovy/com/farpost/geb/SearchTest.groovy
@@ -4,18 +4,23 @@ import com.farpost.logwatcher.cucumber.SearchPage
 import com.farpost.logwatcher.cucumber.SearchResultsPage
 import geb.Browser
 import org.testng.annotations.Test
+import static org.testng.Assert.assertFalse
 import static org.testng.Assert.assertTrue
 
 public class SearchTest extends GebFunctionalTestSuite {
 
 	@Test
-	public void searchTest() {
+	public void searchByApplicationTest() {
+		getLogger("Foo").error("foo error")
+		getLogger("Bar").error("bar error")
+
 		Browser.drive(driver, applicationUri) {
 			to SearchPage
-			searchField.value("at: frontend")
+			searchField.value("at: Foo")
 			search()
 			waitFor { at(SearchResultsPage) }
-			assertTrue result(0).contains("very 'very' very")
+			assertTrue result(0).contains("foo error")
+			assertFalse result(0).contains("bar error")
 		}
 	}
 
