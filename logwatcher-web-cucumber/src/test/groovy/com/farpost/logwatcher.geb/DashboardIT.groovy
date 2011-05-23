15717274d839143675d771579c00a0a275d6c9f5
Alexander Zolotov <zolotov@farpost.com>
2011-05-23 22:06:00 +1100

    Дописал еще 2 теста. Перевел тест на Hamcrest

---
+++ logwatcher-web-cucumber/src/test/groovy/com/farpost/logwatcher.geb/DashboardIT.groovy
@@ -0,0 +1,21 @@
+package com.farpost.logwatcher.geb
+
+import org.testng.annotations.Test
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.hasItem
+import static org.hamcrest.Matchers.not
+
+class DashboardIT extends LogwatcherFunctionalTestSuite {
+
+	@Test
+	public void newApplicationShouldBeShownOnDashboard() {
+		def newAppName = "NewApp"
+
+		to DashboardPage
+		assertThat(applications, not(hasItem(newAppName)))
+
+		getLogger(newAppName).error("New log entry")
+		to DashboardPage
+		assertThat(applications, hasItem(newAppName))
+	}
+}
--- logwatcher-web-cucumber/src/test/groovy/com/farpost/logwatcher.geb/SearchIT.groovy
+++ logwatcher-web-cucumber/src/test/groovy/com/farpost/logwatcher.geb/SearchIT.groovy
@@ -1,8 +1,9 @@
 package com.farpost.logwatcher.geb
 
 import org.testng.annotations.Test
-import static org.testng.Assert.assertFalse
-import static org.testng.Assert.assertTrue
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.containsString
+import static org.hamcrest.Matchers.not
 
 public class SearchIT extends LogwatcherFunctionalTestSuite {
 
@@ -15,8 +16,8 @@ public class SearchIT extends LogwatcherFunctionalTestSuite {
 		searchField.value("at: Foo")
 		search()
 		waitFor { at(SearchResultsPage) }
-		assertTrue result(0).contains("foo error")
-		assertFalse result(0).contains("bar error")
+		assertThat result(0), containsString("foo error")
+		assertThat result(0), not(containsString("bar error"))
 	}
 
 	@Test
@@ -28,8 +29,22 @@ public class SearchIT extends LogwatcherFunctionalTestSuite {
 		searchField.value("caused-by: IllegalArgumentException")
 		search()
 		waitFor { at(SearchResultsPage) }
-		assertTrue result(0).contains("IllegalArgumentException")
-		assertFalse result(0).contains("AssertionError")
+		assertThat result(0), containsString("IllegalArgumentException")
+		assertThat result(0), not(containsString("AssertionError"))
+	}
+
+	@Test
+	public void searchPageSortingTest() {
+		getLogger("Bar").error("First error")
+		sleep(2000)
+		getLogger("Bar").error("Second error")
+
+		to SearchPage
+		searchField.value("at: Bar")
+		search()
+		waitFor { at(SearchResultsPage) }
+		assertThat result(0), containsString("Second error")
+		assertThat result(1), containsString("First error")
 	}
 
 }
