287c8f6a8730d453d3f48c1dc440493adda01946
Alexander Zolotov <zolotov@farpost.com>
2011-05-23 11:49:29 +1100

    Geb test helpers

---
+++ logwatcher-web-cucumber/src/test/groovy/geb/testng/GebTest.groovy
@@ -0,0 +1,53 @@
+package geb.testng
+
+import org.openqa.selenium.WebDriver
+import geb.Browser
+import org.testng.annotations.AfterTest
+
+class GebTest {
+
+	private _browser
+
+	Browser getBrowser() {
+		if (_browser == null) {
+			_browser = createBrowser()
+		}
+		_browser
+	}
+
+	void resetBrowser() {
+		_browser = null
+	}
+
+	def methodMissing(String name, args) {
+		getBrowser()."$name"(* args)
+	}
+
+	def propertyMissing(String name) {
+		getBrowser()."$name"
+	}
+
+	def propertyMissing(String name, value) {
+		getBrowser()."$name" = value
+	}
+
+	Browser createBrowser() {
+		def driver = createDriver()
+		def baseUrl = getBaseUrl()
+
+		driver ? new Browser(driver, baseUrl) : new Browser(baseUrl)
+	}
+
+	WebDriver createDriver() {
+		null // use Browser default
+	}
+
+	String getBaseUrl() {
+		null
+	}
+
+	@AfterTest
+	void clearBrowserCookies() {
+		browser.clearCookiesQuietly()
+	}
+}
