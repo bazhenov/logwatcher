636f2e52530ad423b31fb9755f821c7c36cac763
Denis Bazhenov <bazhenov@farpost.com>
2011-06-09 11:50:35 +1100

    LW-88: OOM при переоткрытии index reader'а
    - теперь кеш идентификаторов загружается как int[]
    - вынес номер порта для тестирование logback appender'а в property 'it.udp-appender.port'

--- logwatcher-core/src/main/java/com/farpost/logwatcher/storage/SearcherReference.java
+++ logwatcher-core/src/main/java/com/farpost/logwatcher/storage/SearcherReference.java
@@ -4,18 +4,20 @@ import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.Searcher;
 
 /**
- * Tuple хранящий ссылки на {@link org.apache.lucene.search.IndexSearcher} и связанный с ним {@link org.apache.lucene.search.FieldCache} по полю id
+ * Tuple хранящий ссылки на {@link org.apache.lucene.search.IndexSearcher} и связанный с ним
+ * {@link org.apache.lucene.search.FieldCache} по полю id
  * <p/>
- * Для корректного закрытия {@link org.apache.lucene.search.IndexSearcher}'а и {@link org.apache.lucene.index.IndexReader}'а, используется финализация.
+ * Для корректного закрытия {@link org.apache.lucene.search.IndexSearcher}'а и
+ * {@link org.apache.lucene.index.IndexReader}'а, используется финализация.
  */
 final class SearcherReference {
 
 	private final IndexReader indexReader;
 
 	private final Searcher searcher;
-	private final String[] idFieldCache;
+	private final int[] idFieldCache;
 
-	SearcherReference(IndexReader indexReader, Searcher searcher, String[] idFieldCache) {
+	SearcherReference(IndexReader indexReader, Searcher searcher, int[] idFieldCache) {
 		this.indexReader = indexReader;
 		this.searcher = searcher;
 		this.idFieldCache = idFieldCache;
@@ -25,7 +27,7 @@ final class SearcherReference {
 		return searcher;
 	}
 
-	String[] getIdFieldCache() {
+	int[] getIdFieldCache() {
 		return idFieldCache;
 	}
 
