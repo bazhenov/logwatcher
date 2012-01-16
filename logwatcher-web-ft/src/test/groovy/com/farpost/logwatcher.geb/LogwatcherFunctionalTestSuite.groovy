72d7cc56350d85215bc8951dc937c5d7efb83fd1
Denis Bazhenov <bazhenov@farpost.com>
2012-01-16 12:20:28 +1100

    - небольшой рефакторинг в LogwatcherFunctionalTestSuite.groovy - исправления в дескрипторе проекта

--- logwatcher-web-ft/src/test/groovy/com/farpost/logwatcher.geb/LogwatcherFunctionalTestSuite.groovy
+++ logwatcher-web-ft/src/test/groovy/com/farpost/logwatcher.geb/LogwatcherFunctionalTestSuite.groovy
@@ -1,7 +1,6 @@
 package com.farpost.logwatcher.geb
 
 import ch.qos.logback.classic.Level
-import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.Appender
 import com.farpost.logwatcher.logback.LogWatcherAppender
@@ -11,7 +10,10 @@ import geb.testng.GebReportingTest
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.testng.annotations.AfterSuite
+import static org.slf4j.LoggerFactory.getLogger
 
 abstract class LogwatcherFunctionalTestSuite extends GebReportingTest {
 
@@ -20,7 +22,7 @@ abstract class LogwatcherFunctionalTestSuite extends GebReportingTest {
 	private static final int port = 6578
 	private static final WebDriver driver;
 	private HashMap<String, Appender<ILoggingEvent>> appenders = new HashMap<String, Appender<ILoggingEvent>>()
-	Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(LogwatcherFunctionalTestSuite.class)
+	protected Logger logger = getLogger(getClass())
 
 	@Override
 	Configuration createConf() {
@@ -32,7 +34,7 @@ abstract class LogwatcherFunctionalTestSuite extends GebReportingTest {
 	}
 
 	private WebDriver createDriver() {
-		if(driver == null) {
+		if (driver == null) {
 			if (System.getProperty("browser").equals("firefox")) {
 				logger.info("Firefox driver selected")
 				driver = new FirefoxDriver()
@@ -53,13 +55,12 @@ abstract class LogwatcherFunctionalTestSuite extends GebReportingTest {
 		}
 	}
 
-
 	File getReportDir() {
 		File file = new File(REPORT_DIRECTORY)
 		if (file.exists() || file.mkdirs()) {
 			return file
 		} else {
-			logger.warn("Can't create " + REPORT_DIRECTORY + " directory for test reports")
+			logger.warn("Can't create {} directory for test reports", REPORT_DIRECTORY)
 			return null;
 		}
 	}
@@ -77,8 +78,9 @@ abstract class LogwatcherFunctionalTestSuite extends GebReportingTest {
 		}
 	}
 
-	protected Logger getLogger(String applicationId) {
-		Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(applicationId)
+	protected ch.qos.logback.classic.Logger getLogger(String applicationId) {
+		//noinspection GroovyAssignabilityCheck
+		ch.qos.logback.classic.Logger logger = LoggerFactory.getLogger(applicationId)
 		logger.setLevel(Level.DEBUG)
 		if (logger.getAppender(applicationId) == null) {
 			logger.addAppender(getAppender(applicationId))
