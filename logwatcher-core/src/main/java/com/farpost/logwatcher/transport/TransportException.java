76ba0d749b4fbe2923bdf63ce7ad5b0da0ff7f6a
Alexander Zolotov <zolotov@farpost.com>
2012-02-07 12:37:09 +1100

    throw exception on queue overflow

--- logwatcher-core/src/main/java/com/farpost/logwatcher/transport/TransportException.java
+++ logwatcher-core/src/main/java/com/farpost/logwatcher/transport/TransportException.java
@@ -2,7 +2,11 @@ package com.farpost.logwatcher.transport;
 
 public class TransportException extends Exception {
 
-	public TransportException(Throwable cause) {
+    public TransportException(String message) {
+        super(message);
+    }
+
+    public TransportException(Throwable cause) {
 		super(cause);
 	}
 }
--- logwatcher-core/src/test/java/com/farpost/logwatcher/transport/WriteToChannelTransportListenerTest.java
+++ logwatcher-core/src/test/java/com/farpost/logwatcher/transport/WriteToChannelTransportListenerTest.java
@@ -16,26 +16,36 @@ import static org.joda.time.DateTime.now;
 
 public class WriteToChannelTransportListenerTest {
 
-	private final Marshaller marshaller = new Jaxb2Marshaller();
+    private final Marshaller marshaller = new Jaxb2Marshaller();
 
-	private QueueChannel messageChannel;
-	private TransportListener listener;
+    private QueueChannel messageChannel;
+    private TransportListener listener;
 
-	@BeforeMethod
-	public void setUp() {
-		messageChannel = new QueueChannel(1);
-		listener = new WriteToChannelTransportListener(messageChannel);
-	}
+    @BeforeMethod
+    public void setUp() {
+        messageChannel = new QueueChannel(1);
+        listener = new WriteToChannelTransportListener(messageChannel);
+    }
 
-	@Test
-	public void listenerShouldWriteEntryToDatabase() throws TransportException, InterruptedException {
-		Cause cause = new Cause("type", "message", "stack");
-		LogEntry entry = new LogEntryImpl(now(), "group", "message", error, "checksum", "default", null, cause);
+    @Test
+    public void listenerShouldWriteEntryToDatabase() throws TransportException, InterruptedException {
+        Cause cause = new Cause("type", "message", "stack");
+        LogEntry entry = new LogEntryImpl(now(), "group", "message", error, "checksum", "default", null, cause);
 
-		byte[] message = marshaller.marshall(entry);
-		listener.onMessage(message);
+        byte[] message = marshaller.marshall(entry);
+        listener.onMessage(message);
 
-		byte[] actualMessage = (byte[]) messageChannel.receive().getPayload();
-		assertThat(actualMessage, equalTo(message));
-	}
+        byte[] actualMessage = (byte[]) messageChannel.receive().getPayload();
+        assertThat(actualMessage, equalTo(message));
+    }
+
+    @Test(expectedExceptions = TransportException.class)
+    public void listenerShouldThrowExceptionOnQueueOverflow() throws TransportException, InterruptedException {
+        Cause cause = new Cause("type", "message", "stack");
+        LogEntry entry = new LogEntryImpl(now(), "group", "message", error, "checksum", "default", null, cause);
+
+        byte[] message = marshaller.marshall(entry);
+        listener.onMessage(message);
+        listener.onMessage(message);
+    }
 }
