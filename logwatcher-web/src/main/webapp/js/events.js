4c0009fd749bfb9de3bec82d5c2ab2168306554f
Denis Bazhenov <bazhenov@farpost.com>
2013-06-27 13:09:42 +1100

    Реализовал новый контрол отображающий count

--- logwatcher-web/src/main/webapp/js/events.js
+++ logwatcher-web/src/main/webapp/js/events.js
@@ -4,13 +4,13 @@ function changeSeverity(url) {
 	$.ajax({
 		type: 'GET',
 		url: url,
-		success: function() {
-			window.location="";
+		success: function () {
+			window.location = "";
 		}
 	});
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 	function toggleEntry(entry) {
 		entry.toggleClass('selectedEntry');
 		var checksum = entry.attr("checksum");
@@ -19,7 +19,7 @@ $(document).ready(function() {
 		if (content.attr("loaded") == "false") {
 			content.attr("loaded", "true");
 			entry.addClass("loadingContent");
-			content.load("/service/content", {'checksum': checksum, 'date': lastOccurredDate}, function(response, code) {
+			content.load("/service/content", {'checksum': checksum, 'date': lastOccurredDate}, function (response, code) {
 				if (code != "success") {
 					content.html("");
 				}
@@ -29,7 +29,7 @@ $(document).ready(function() {
 		}
 	}
 
-	$('.givenQuery').click(function(target) {
+	$('.givenQuery').click(function (target) {
 		var el = $(target.target).parents('.givenQuery');
 		var query = el.attr('rawQuery');
 		el.html("<form action='/search'><input id='searchInput' type='text' name='q' value='" + query + "' /></form>");
@@ -37,13 +37,13 @@ $(document).ready(function() {
 		el.unbind('click');
 	});
 
-	$('.entryHeader').click(function(target) {
+	$('.entryHeader').click(function (target) {
 		if ($(target.target).parents(".noBubble").length <= 0) {
 			toggleEntry($(this).parents(".entry"));
 		}
 	});
 
-	$('a.removeEntry').live('click', function() {
+	$('a.removeEntry').live('click', function () {
 		var entry = $(this).parents(".entry");
 		var checksum = entry.attr('checksum');
 		if (confirm('Are you shure you want to remove this entry?')) {
@@ -51,13 +51,13 @@ $(document).ready(function() {
 			$.ajax({
 				type: "GET",
 				url: '/entry/remove?checksum=' + checksum,
-				complete: function() {
+				complete: function () {
 					entry.removeClass('removing');
 				},
-				success: function(msg) {
+				success: function (msg) {
 					entry.hide('normal');
 				},
-				error: function(request, status, error) {
+				error: function (request, status, error) {
 					alert("There is an error occurred while removing entry: " + status + ", " + error)
 				}
 			})
@@ -68,30 +68,30 @@ $(document).ready(function() {
 
 	var checksum = location.hash.substring(1);
 	if (checksum.length > 0) {
-		$('.entry').each(function() {
+		$('.entry').each(function () {
 			if ($(this).attr('checksum') == checksum) {
 				toggleEntry($(this));
 			}
 		});
 	}
 
-	$('#applicationId').click(function() {
+	$('#applicationId').click(function () {
 		showPopup($(this), $('#applicationSelector'));
 		return false;
 	});
 
-	$('#pieChartLink').click(function() {
+	$('#pieChartLink').click(function () {
 		showPopup($(this), $('#pieChart'));
 		return false;
 	});
 
 
-	$('#date').click(function() {
+	$('#date').click(function () {
 		showPopup($(this), $('#dateSelector'));
 		return false;
 	});
 
-	$('body').click(function(event) {
+	$('body').click(function (event) {
 		if ($(event.target).parents(".popupContainer").length <= 0) {
 			hideAllPopups();
 		}
@@ -116,22 +116,114 @@ $(document).ready(function() {
 	}
 
 	/* Open In IDE */
-	$('.entryContent pre').each(function() {
+	$('.entryContent pre').each(function () {
 		var text = $(this).html().replace(/[0-9a-z_A-Z\-\.\/]+:\d+/g, '<a class="openinide-link" href="#$&">$&</a>');
 		$(this).html(text);
 	});
 
-	$('.logEntry pre').each(function() {
+	$('.logEntry pre').each(function () {
 		var text = $(this).html().replace(/[0-9a-z_A-Z\-\.\/]+:\d+/g, '<a class="openinide-link" href="#$&">$&</a>');
 		$(this).html(text);
 	});
 
-	$('a.openinide-link').click(function(e) {
+	$('a.openinide-link').click(function (e) {
 		e.preventDefault();
 		var message = $(this).attr("href").substr(1);
-		$.getJSON('http://localhost:8091/?message=' + message + '&callback=?', function(json) {
+		$.getJSON('http://localhost:8091/?message=' + message + '&callback=?', function (json) {
 			//do nothing
 		});
 	});
-
 });
+
+function Counterina(canvas, maxData) {
+	this.canvas = canvas;
+	this.maxData = maxData > 1 ? maxData : 2;
+
+	this.formatNumber = function (count) {
+		if (count >= 1000000) {
+			return Math.round(count / 1000000) + "M";
+		} else if (count >= 100000) {
+			return Math.round(count / 1000) + "K";
+		} else if (count.toString().length == 5) {
+			return count.substring(0, 2) + " " + count.substring(2);
+		} else if (count.toString().length == 4) {
+			return count.substring(0, 1) + " " + count.substring(1);
+		}
+		return count;
+	};
+
+	this.hypLength = function (c1, c2) {
+		return Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
+	};
+
+	this.draw = function (count, severity) {
+		var context = canvas.getContext('2d');
+
+		var x = canvas.width / 2;
+		var y = canvas.height / 2;
+		var text = this.formatNumber(count);
+		var rMin = 3;
+		var rMax = canvas.width / 2;
+		var radius = (Math.sqrt(count - 1) * (rMax - rMin)) / (Math.sqrt(this.maxData - 1)) + rMin;
+
+		context.font = 'bold 12pt Helvetica Neue';
+		context.textAlign = 'center';
+		context.textBaseline = 'middle';
+
+		var metrics = context.measureText(text);
+
+		var width = metrics.width;
+		var textRadius = this.hypLength(width / 2, 12 / 2);
+		var offset = radius - textRadius;
+
+		switch (severity) {
+			case 'warn':
+				context.fillStyle = 'rgb(215, 87, 0)';
+				break;
+
+			case 'info':
+				context.fillStyle = 'rgb(51, 153, 0)';
+				break;
+
+			case 'debug':
+				context.fillStyle = 'rgb(34, 68, 119)';
+				break;
+
+			case 'trace':
+				context.fillStyle = 'rgb(170, 170, 170)';
+				break;
+
+			default:
+				context.fillStyle = 'rgb(153, 0, 0)';
+				break;
+		}
+
+		if (offset <= 0) {
+			// текст не вмещается в круг
+			context.beginPath();
+			y = (canvas.height - 10) / 2;
+			context.arc(x, y, radius, 0, 2 * Math.PI, false);
+			context.closePath();
+			context.fill();
+
+			context.font = 'bold 10pt Helvetica Neue';
+			context.textBaseline = 'bottom';
+			context.fillText(text, x, canvas.height);
+		} else {
+			// текст вмещается в круг
+			context.beginPath();
+			context.arc(x, y, radius, 0, 2 * Math.PI, false);
+			context.closePath();
+			context.fill();
+
+			if (offset > 5) {
+				context.fillStyle = 'white';
+				context.fillText(text, x, y);
+			} else {
+				context.font = 'bold 10pt Helvetica Neue';
+				context.fillStyle = 'white';
+				context.fillText(text, x, y);
+			}
+		}
+	}
+}
