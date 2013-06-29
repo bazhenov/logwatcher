b2f0a0fd2c796fbe6fa7fcb167df5a9e41814b81
Denis Bazhenov <bazhenov@farpost.com>
2013-06-29 13:30:33 +1100

    Detailed view reworked

--- logwatcher-web/src/main/java/com/farpost/logwatcher/web/Functions.java
+++ logwatcher-web/src/main/java/com/farpost/logwatcher/web/Functions.java
@@ -1,5 +1,7 @@
 package com.farpost.logwatcher.web;
 
+import com.farpost.logwatcher.Cause;
+
 import static java.lang.Math.abs;
 
 public class Functions {
@@ -21,5 +23,30 @@ public class Functions {
 		} else {
 			return className;
 		}
+	}
+
+	public static String formatCause(Cause rootCause) {
+		if (rootCause == null) {
+			return "";
+		}
+		StringBuilder prefix = new StringBuilder();
+		StringBuilder stackTrace = new StringBuilder();
+
+		Cause cause = rootCause;
+		while (cause != null) {
+			if (cause != rootCause) {
+				stackTrace.append("\n\n").append(prefix).append("Caused by ");
+			}
+			String iStack = cause.getStackTrace().replaceAll("\n", "\n" + prefix);
+			stackTrace.append(cause.getType())
+				.append(": ")
+				.append(cause.getMessage())
+				.append("\n")
+				.append(prefix)
+				.append(iStack);
+			cause = cause.getCause();
+			prefix.append("  ");
+		}
+		return stackTrace.toString();
 	}
 }
