0c90b7c113fbe4f5a18780b09afa0a92da6fab11
Denis Bazhenov <bazhenov@farpost.com>
2013-07-04 15:46:56 +1100

    search interface improvements

--- logwatcher-web/src/main/webapp/js/events.js
+++ logwatcher-web/src/main/webapp/js/events.js
@@ -11,37 +11,6 @@ function changeSeverity(url) {
 }
 
 $(document).ready(function () {
-	$('.givenQuery').click(function (target) {
-		var el = $(target.target).parents('.givenQuery');
-		var query = el.attr('rawQuery');
-		el.html("<form action='/search'><input id='searchInput' type='text' name='q' value='" + query + "' /></form>");
-		$('#searchInput').focus();
-		el.unbind('click');
-	});
-
-	$('a.removeEntry').live('click', function () {
-		var entry = $(this).parents(".entry");
-		var checksum = entry.attr('checksum');
-		if (confirm('Are you shure you want to remove this entry?')) {
-			entry.addClass('removing');
-			$.ajax({
-				type: "GET",
-				url: '/entry/remove?checksum=' + checksum,
-				complete: function () {
-					entry.removeClass('removing');
-				},
-				success: function (msg) {
-					entry.hide('normal');
-				},
-				error: function (request, status, error) {
-					alert("There is an error occurred while removing entry: " + status + ", " + error)
-				}
-			})
-		}
-
-		return false;
-	});
-
 	$('#applicationId').click(function () {
 		showPopup($(this), $('#applicationSelector'));
 		return false;
