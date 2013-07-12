c3fd381e6ff0cc4c8a9a8fa3eddd3b9ec5813ef5
Denis Bazhenov <bazhenov@farpost.com>
2013-07-12 21:22:20 +1100

    Jetty distribution added

---
+++ distribute.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mvn -q package
+cp -r jetty-distribution logwatcher-$1
+cp logwatcher-web/target/logwatcher-web-2.0.war logwatcher-$1/webapps/ROOT.war
+tar -czf logwatcher-$1.tar.gz logwatcher-$1
+rm -rf logwatcher-$1
