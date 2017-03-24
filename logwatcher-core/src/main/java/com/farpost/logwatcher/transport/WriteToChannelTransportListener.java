29252ed6b90a8feee4e05193c28c0ee9837690f4
Maxim Glazkov <glazkov.maxim@gmail.com>
2017-03-24 16:35:45 +1000

    BZR-20289

--- logwatcher-core/src/main/java/com/farpost/logwatcher/transport/WriteToChannelTransportListener.java
+++ logwatcher-core/src/main/java/com/farpost/logwatcher/transport/WriteToChannelTransportListener.java
@@ -5,10 +5,13 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 
 import java.net.InetAddress;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class WriteToChannelTransportListener implements TransportListener {
+
+	public static AtomicLong rejectedByOverflowCount = new AtomicLong();
 
 	public static final String SENDER_ADDRESS = "SenderAddress";
 	private final MessageChannel messageChannel;
@@ -24,6 +27,7 @@ public class WriteToChannelTransportListener implements TransportListener {
 			.setHeader(SENDER_ADDRESS, sender)
 			.build();
 		if (!messageChannel.send(enveloper, 0)) {
+			rejectedByOverflowCount.incrementAndGet();
 			throw new TransportException("Queue overflowed");
 		}
 	}
--- logwatcher-web/src/main/java/com/farpost/logwatcher/web/controller/SystemController.java
+++ logwatcher-web/src/main/java/com/farpost/logwatcher/web/controller/SystemController.java
@@ -5,6 +5,7 @@ import com.farpost.logwatcher.LogEntry;
 import com.farpost.logwatcher.LogEntryImpl;
 import com.farpost.logwatcher.Severity;
 import com.farpost.logwatcher.marshalling.Jaxb2Marshaller;
+import com.farpost.logwatcher.transport.WriteToChannelTransportListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,5 +66,11 @@ public class SystemController {
 			socket.send(packet);
 		}
 		return "Ok";
+	}
+
+	@RequestMapping("/rejected_by_overflow")
+	@ResponseBody
+	public long rejectedByOverflow() {
+		return WriteToChannelTransportListener.rejectedByOverflowCount.get();
 	}
 }
